83ad5285e7edceeb0714eaf4ad391ebc3bd34f06
Diego <daspking@gmail.com>
2025-07-31 21:59:55 -0400

    Trying to fix deploy

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -2,20 +2,14 @@ import { EmbedGenerator } from '@/components/EmbedGenerator';
 import { Metadata } from 'next';
 import { siteMetadata } from '@/lib/config';
 
-const siteUrl = 'https://xdasp.me';
-
-export function generateMetadata({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}): Metadata {
-  const title = Array.isArray(searchParams.title) ? searchParams.title[0] : searchParams.title || siteMetadata.title;
-  const description = Array.isArray(searchParams.description) ? searchParams.description[0] : searchParams.description || siteMetadata.description;
-  const image = Array.isArray(searchParams.image) ? searchParams.image[0] : searchParams.image || siteMetadata.image;
-  const color = Array.isArray(searchParams.color) ? searchParams.color[0] : searchParams.color || '#a855f7';
+export function generateMetadata({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }): Metadata {
+  const title = Array.isArray(searchParams.title) ? searchParams.title[0] : searchParams.title ?? siteMetadata.title;
+  const description = Array.isArray(searchParams.description) ? searchParams.description[0] : searchParams.description ?? siteMetadata.description;
+  const image = Array.isArray(searchParams.image) ? searchParams.image[0] : searchParams.image ?? siteMetadata.image;
+  const color = Array.isArray(searchParams.color) ? searchParams.color[0] : searchParams.color ?? '#a855f7';
 
   return {
-    metadataBase: new URL(siteUrl),
+    metadataBase: new URL('https://xdasp.me'),
     title,
     description,
     openGraph: {
